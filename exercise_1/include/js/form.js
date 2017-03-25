4ba7aa5a09c62b53ca78784dc92f63f749711735
abhijeet7666 <abhijeet7654@gmail.com>
2017-03-25 21:06:16 +0530

    add ajax request

--- exercise_1/include/js/form.js
+++ exercise_1/include/js/form.js
@@ -1,33 +1,54 @@
-var inputJSON = [
-  {
-    "userId": 1,
-    "id": 1,
-    "title": "title 1",
-    "body": "body 1"
-  },
-  {
-    "userId": 2,
-    "id": 2,
-    "title": "title 2",
-    "body": "body 2"
-  }
-];
-
 $(function() {
 	
-	// element selectors
-	$form_index 		= $(document.getElementById("index_id"));
-	$form_user_id 		= $(document.getElementById("user_id"));
-	$form_user_title 	= $(document.getElementById("user_title"));
-	$form_user_body 	= $(document.getElementById("user_body"));
+	// define variables
+	var json_srcpath = "http://jsonplaceholder.typicode.com/posts";
+	var inputJSON = {};
 	
+	// element selectors
+	var $form_index 		= $(document.getElementById("index_id")),
+		$form_user_id 		= $(document.getElementById("user_id")),
+		$form_user_title 	= $(document.getElementById("user_title")),
+		$form_user_body 	= $(document.getElementById("user_body"));
+		
 	// render form elements
-	for(var i=0, len = inputJSON.length; i<len ;i++){
-		$form_index.append("<option value="+inputJSON[i]['id']+" >"+inputJSON[i]['id']+"</option>");
+	function loadFormElements(){
+		
+		for(var i=0, len = inputJSON.length; i<len ;i++){
+			$form_index.append("<option value="+inputJSON[i]['id']+" >"+inputJSON[i]['id']+"</option>");
+		}
+		
+		$form_user_id.val(inputJSON[0]['userId']);
+		$form_user_title.val(inputJSON[0]['title']);
+		$form_user_body.val(inputJSON[0]['body']);		
 	}
 	
-	$form_user_id.val(inputJSON[0]['userId']);
-	$form_user_title.val(inputJSON[0]['title']);
-	$form_user_body.val(inputJSON[0]['body']);	
+	// update form elements
+	function reloadFormElements(idx){
+		 
+		$form_user_id.val(inputJSON[idx]['userId']);
+		$form_user_title.val(inputJSON[idx]['title']);
+		$form_user_body.val(inputJSON[idx]['body']);
+		
+	}
+
+	// get json data
+	$.ajax({
+	  method: "GET",
+	  url: json_srcpath,
+	  success: function(data){
+		 inputJSON = data;
+		 loadFormElements();
+	  },
+	  error: function(){
+		  alert("unable to fetch data");
+	  }
+	});
+	
+	// attach events
+	$form_index.on('change', function(){
+		var id = $(this).val();
+		reloadFormElements(id);
+	});
+		
 	
 });
